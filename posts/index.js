5ac0fd48d137c9ef1e65e7bec5b07aa537237405
ista1024 <ista1024@gmail.com>
2022-03-30 08:43:15 +0900

    blog post utility

--- posts/index.js
+++ posts/index.js
@@ -1,5 +1,19 @@
 import matter from "gray-matter";
 
+export const postSlugs = () =>
+  ((context) => {
+    return context
+      .keys()
+      .map((key) => key.replace(/^.*[\\\/]/, "").slice(0, -3));
+  })(require.context("./", true, /\.md$/));
+
+export const postForSlug = async (slug) => {
+  const document = await import(`./${slug}.md`);
+  const { data: frontmatter, content: body } = matter(document.default);
+
+  return { frontmatter, body, slug };
+};
+
 export const posts = () =>
   ((context) => {
     const keys = context.keys();
