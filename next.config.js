280b15febe02214ad6ad3c6550b4a9338d6b6e3e
ista1024 <ista1024@gmail.com>
2022-03-29 09:19:36 +0900

    install raw-loader & gray-matter

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   target: "serverless",
   reactStrictMode: true,
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
