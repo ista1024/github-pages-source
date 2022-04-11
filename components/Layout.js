33f62da15db0bda7eb2dd8128cf841a35531ba19
ista1024 <ista1024@gmail.com>
2022-04-11 11:29:34 +0900

    add reference

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ function Layout({ children, pageTitle }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{pageTitle}</title>
       </Head>
+      {/* https://www.kalopilato.com/blog/how-to-build-and-deploy-your-personal-blog-with-next-js */}
       <div className="flex flex-col min-h-screen">
         <header className="w-full h-16 border-b border-purple-500 flex items-center justify-center">
           <div className="w-11/12 md:w-full max-w-3xl flex flex-row justify-between">
--- pages/about.js
+++ pages/about.js
@@ -6,6 +6,7 @@ function About() {
       <article className="prose max-w-none">
         <h1>About</h1>
         <p>{`Hello, I'm a blogger and I blog about interesting things.`}</p>
+        <p>{`https://www.kalopilato.com/blog/how-to-build-and-deploy-your-personal-blog-with-next-js`}</p>
       </article>
     </Layout>
   );
