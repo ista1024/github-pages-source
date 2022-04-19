9a2f400dd5869d78a00273ef78ee6f593cfb9b62
ista1024 <ista1024@gmail.com>
2022-04-19 15:20:48 +0900

    modify about

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,8 @@ function About() {
       <article className="prose max-w-none">
         <h1>About</h1>
         <p>{`Hello, I'm a blogger and I blog about interesting things.`}</p>
-        <p>{`https://www.kalopilato.com/blog/how-to-build-and-deploy-your-personal-blog-with-next-js`}</p>
+        <p>{`https://github.com/ista1024`}</p>
+        <p>{`https://ista1024.github.io`}</p>
       </article>
     </Layout>
   );
