1f6fb696fcda1de3b8f34b0d2cf2c2193358cd0d
ista1024 <ista1024@gmail.com>
2022-04-19 17:00:19 +0900

    added assetsPrefix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   target: "serverless",
   reactStrictMode: true,
+  assetPrefix: "https://ista1024.github.io/",
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
